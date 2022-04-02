50070b90286a9aac125475be9bf937d3fd3db963
CadeYu <yu83612457@gmail.com>
2022-04-02 22:54:42 +0800

    代码重构

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ client.on('interactionCreate', async interaction => {
         console.error(error);
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
+
+
+
 });
 
 client.login(token);
